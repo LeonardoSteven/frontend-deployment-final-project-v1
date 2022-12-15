1d20c3d6a52bc8687b48826042c84d61e6e75e93
Leonardo Steven <leonardo.steven@student.umn.ac.id>
2022-12-15 16:48:42 +0700

    second commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://deployment-leonardo-fp-db235.web.app" // TODO: replace this
 const studentName = "Leonardo Steven" // TODO: replace this
 const studentId = "FE3023627" // TODO: replace this
 
